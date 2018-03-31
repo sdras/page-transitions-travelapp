61e47c7cfecaa73055f54c82fd21d5cbd67aeebd
sdras <sarah.drasner@gmail.com>
2018-03-31 13:48:00 -0600

    animate header

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -1,78 +1,89 @@
 <template>
-  <nav>
-    <ul>
-      <nuxt-link exact to="/"><li>{{ users[0].name | firstName }}'s Home</li></nuxt-link>
-      <nuxt-link to="/place"><li>{{ users[0].name | firstName }}'s Places</li></nuxt-link>
-      <nuxt-link to="/group"><li>{{ users[0].name | firstName }}'s Group Trips</li></nuxt-link>
-    </ul>
-    <icon-base class="menu" icon-name="menu" icon-color="white" width="28" height="28">
-      <icon-three-dot />
-    </icon-base>
-    <transition-group :class="{ 'place' : (page === 'place'), 'group' : (page === 'group') }" tag="div">
-      <div class="profile-photo" key="profile">
-        <img :src="users[0].img" />
-        <div class="online"></div>
-      </div>
-      <div class="profile-photo-secondary two" key="profile2">
-        <img src="/profile3.jpg" />
-        <div class="online"></div>
-      </div>
-      <div class="profile-photo-secondary three" key="profile3">
-        <img src="/profile4.jpg" />
-        <div class="online"></div>
-      </div>
-      <div class="profile-photo-secondary four" key="profile4">
-        <img src="/profile5.jpg" />
-        <div class="online"></div>
-      </div>
-      <button key="follow" class="follow">Follow</button>
-      <h2 key="profile-name" class="profile-name">
-        <span v-if="page === 'group'" class="user-trip">{{ users[0].trips[0] }}</span>
-        <span v-else>{{ users[0].name }}</span>
-      </h2>
-      <div class="side-icon" key="sideicon">
-        <icon-base 
-          v-if="page === 'index'"
-          icon-name="mail" 
-          icon-color="white" 
-          width="22" 
-          height="22"
-        >
-          <icon-mail />
-        </icon-base>
-        <icon-base 
-          v-else
-          icon-name="plus" 
-          width="18" 
-          height="18"
-        >
-          <icon-plus />
-        </icon-base>
-      </div>
-      <aside key="aside">
-        <p class="map-pin">
-          <icon-base 
-            icon-name="mappin" 
-            width="18" 
-            height="18"
-          >
-            <icon-map-pin />
-          </icon-base>
-          British Overseas Territory
-        </p>
-        <p class="calendar">
-          <icon-base 
-            icon-name="calendar" 
-            width="18" 
-            height="18"
-          >
-            <icon-calendar />
-          </icon-base>
-          {{ users[0].days }} days traveling
-        </p>
-      </aside>
+  <header :class="{ 'place' : (page === 'place'), 'group' : (page === 'group') }">
+
+    <transition-group name="bk" tag="div" class="bk-img">
+      <div key="img1" v-if="page === 'index'" class="header-img1"></div>
+      <div key="img2" v-else-if="page === 'place'" class="header-img2"></div>
+      <div key="img3" v-else class="header-img3"></div>
     </transition-group>
-  </nav>
+
+    <div class="nav-wrapper">
+      <nav>
+        <ul>
+          <nuxt-link exact to="/"><li>{{ users[0].name | firstName }}'s Home</li></nuxt-link>
+          <nuxt-link to="/place"><li>{{ users[0].name | firstName }}'s Places</li></nuxt-link>
+          <nuxt-link to="/group"><li>{{ users[0].name | firstName }}'s Group Trips</li></nuxt-link>
+        </ul>
+        <icon-base class="menu" icon-name="menu" icon-color="white" width="28" height="28">
+          <icon-three-dot />
+        </icon-base>
+        <transition-group tag="div">
+          <div class="profile-photo" key="profile">
+            <img :src="users[0].img" />
+            <div class="online"></div>
+          </div>
+          <div class="profile-photo-secondary two" key="profile2">
+            <img src="/profile3.jpg" />
+            <div class="online"></div>
+          </div>
+          <div class="profile-photo-secondary three" key="profile3">
+            <img src="/profile4.jpg" />
+            <div class="online"></div>
+          </div>
+          <div class="profile-photo-secondary four" key="profile4">
+            <img src="/profile5.jpg" />
+            <div class="online"></div>
+          </div>
+          <button key="follow" class="follow">Follow</button>
+          <h2 key="profile-name" class="profile-name">
+            <span v-if="page === 'group'" class="user-trip">{{ users[0].trips[0] }}</span>
+            <span v-else>{{ users[0].name }}</span>
+          </h2>
+          <div class="side-icon" key="sideicon">
+            <icon-base 
+              v-if="page === 'index'"
+              icon-name="mail" 
+              icon-color="white" 
+              width="22" 
+              height="22"
+            >
+              <icon-mail />
+            </icon-base>
+            <icon-base 
+              v-else
+              icon-name="plus" 
+              width="18" 
+              height="18"
+            >
+              <icon-plus />
+            </icon-base>
+          </div>
+          <aside key="aside">
+            <p class="map-pin">
+              <icon-base 
+                icon-name="mappin" 
+                width="18" 
+                height="18"
+              >
+                <icon-map-pin />
+              </icon-base>
+              British Overseas Territory
+            </p>
+            <p class="calendar">
+              <icon-base 
+                icon-name="calendar" 
+                width="18" 
+                height="18"
+              >
+                <icon-calendar />
+              </icon-base>
+              {{ users[0].days }} days traveling
+            </p>
+          </aside>
+        </transition-group>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -104,10 +115,119 @@ export default {
 </script>
 
 <style scoped lang="scss">
+header {
+  width: 100vw;
+  height: 300px;
+  position: relative;
+  // background: url('/header1.jpg') no-repeat center center;
+  // background-size: cover;
+  &:before {
+    content: '';
+    z-index: 10;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: -moz-radial-gradient(
+      center,
+      ellipse cover,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 36%,
+      rgba(0, 0, 0, 0.65) 100%
+    ); /* FF3.6-15 */
+    background: -webkit-radial-gradient(
+      center,
+      ellipse cover,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 36%,
+      rgba(0, 0, 0, 0.65) 100%
+    ); /* Chrome10-25,Safari5.1-6 */
+    background: radial-gradient(
+      ellipse at center,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 36%,
+      rgba(0, 0, 0, 0.65) 100%
+    ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient(
+        startColorstr='#00000000',
+        endColorstr='#a6000000',
+        GradientType=1
+      ); /* IE6-9 fallback on horizontal gradient */
+    opacity: 0.6;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: #000;
+  }
+}
+
+@mixin header ($imgurl) {
+  background: url($imgurl) center bottom fixed;
+  background-size: cover;
+  position: absolute;
+  width: 100vw;
+  height: 300px;
+}
+
+.header-img1 {
+  @include header('/header1.jpg');
+}
+
+.header-img2 {
+  @include header('/header2.jpg');
+}
+
+.header-img3 {
+  @include header('/header3.jpg');
+}
+
+h1 {
+  position: absolute;
+  z-index: 3000;
+  color: white;
+}
+
+.show {
+  position: absolute;
+  z-index: 3000;
+}
+
+.bk-enter-active,
+.bk-leave-active {
+  transition: all 0.4s ease;
+}
+
+.bk-enter,
+.bk-leave-to {
+  transform: scale(1.1);
+  opacity: 0;
+}
+
+.bk-img {
+  position: absolute;
+  width: 100vw;
+  height: 300px;
+  overflow: hidden;
+  top: 0;
+}
+
+.nav-wrapper {
+  width: 100vw;
+  position: relative;
+  z-index: 1000;
+  background: rgba(0, 0, 0, 0.4);
+}
+
 ul {
   list-style: none;
   padding: 15px 0;
-  background: rgba(0, 0, 0, 0.4);
   li {
     display: inline-block;
     margin-right: 40px;
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,6 @@
 <script>
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
