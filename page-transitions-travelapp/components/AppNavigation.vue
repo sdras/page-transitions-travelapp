f96001345b39e91bba3900bec299bd31e6950463
sdras <sarah.drasner@gmail.com>
2018-03-30 23:30:02 -0600

    add in group animations

--- page-transitions-travelapp/components/AppNavigation.vue
+++ page-transitions-travelapp/components/AppNavigation.vue
@@ -5,11 +5,22 @@
       <nuxt-link to="/place"><li>Sophia's Places</li></nuxt-link>
       <nuxt-link to="/group"><li>Sophia's Group Trips</li></nuxt-link>
     </ul>
-    <transition-group :class="{ 'place' : (page === 'place') }" tag="div">
-      <div class="profile-photo" 
-        key="profile"
-      >
+    <transition-group :class="{ 'place' : (page === 'place'), 'group' : (page === 'group') }" tag="div">
+      <div class="profile-photo" key="profile">
         <img src="/profile2.jpg" />
+        <div class="online"></div>
+      </div>
+      <div class="profile-photo-secondary two" key="profile2">
+        <img src="/profile3.jpg" />
+        <div class="online"></div>
+      </div>
+      <div class="profile-photo-secondary three" key="profile3">
+        <img src="/profile4.jpg" />
+        <div class="online"></div>
+      </div>
+      <div class="profile-photo-secondary four" key="profile4">
+        <img src="/profile5.jpg" />
+        <div class="online"></div>
       </div>
       <button key="follow" class="follow">Follow</button>
       <h2 key="profile-name" class="profile-name">Sophia Gonzalez</h2>
@@ -48,37 +59,71 @@ nav {
   position: relative;
 }
 
+@mixin group($top, $left) {
+  position: absolute;
+  top: $top;
+  left: $left;
+  display: block;
+  transition: 0.4s all ease-out;
+}
+
+@mixin online($size, $position, $border) {
+  position: absolute;
+  background: #07dc3c;
+  border-radius: 50% 50%;
+  width: $size;
+  height: $size;
+  right: $position;
+  bottom: $position;
+  border: $border;
+  opacity: 0;
+}
+
 .profile-photo {
   width: 200px;
-  position: absolute;
-  top: 150px;
-  left: 0px;
-  transition: 0.4s all ease;
+  @include group(150px, 0);
   img {
-    width: 100%;
     border-radius: 4px;
+  }
+  .online {
+    @include online(40px, 10px, 2px solid black);
+  }
+}
+
+.profile-photo-secondary {
+  @include group(150px, 0);
+  width: 50px;
+  height: 50px;
+  opacity: 0;
+  transition: none;
+  img {
+    border-radius: 50% 50%;
+  }
+  .online {
+    @include online(10px, 0px, 1px solid black);
+  }
+}
+
+.profile-photo,
+.profile-photo-secondary {
+  img {
+    transition: 0.4s all ease;
+    width: 100%;
   }
 }
 
 .follow {
-  position: absolute;
   font-weight: bold;
   width: 150px;
-  top: 320px;
-  left: 220px;
-  display: block;
-  transition: 0.4s all ease-out;
+  @include group(320px, 220px);
 }
 
 .profile-name {
-  position: absolute;
-  top: 355px;
-  left: 0px;
   font-size: 35px;
-  display: block;
-  transition: 0.4s all ease-out;
+  @include group(355px, 0);
 }
 
+//animations
 .place {
   .follow {
     transform: translate3d(-215px, -80px, 0);
@@ -88,6 +133,44 @@ nav {
   }
   .profile-name {
     transform: translate3d(140px, -125px, 0) scale(0.75);
+    color: white;
+  }
+}
+
+.group {
+  .follow {
+    opacity: 0;
+    transition: none;
+  }
+  .profile-photo {
+    transform: translate3d(-70px, -70px, 0) scale(0.25);
+    img {
+      border-radius: 50% 50%;
+    }
+  }
+  .two {
+    transform: translate3d(65px, 5px, 0);
+    transition: 0.4s all ease-in-out;
+    transition-delay: 0.05s;
+    opacity: 1;
+  }
+  .three {
+    transform: translate3d(120px, 5px, 0);
+    transition: 0.4s all ease-in-out;
+    transition-delay: 0.1s;
+    opacity: 1;
+  }
+  .four {
+    transform: translate3d(175px, 5px, 0);
+    transition: 0.4s all ease-in-out;
+    transition-delay: 0.15s;
+    opacity: 1;
+  }
+  .online {
+    opacity: 1;
+  }
+  .profile-name {
+    transform: translate3d(0px, -125px, 0);
     color: white;
   }
 }
