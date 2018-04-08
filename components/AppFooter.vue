63d800e9c2ed7f4ef39c3db07140815e3ba4edb1
sdras <sarah.drasner@gmail.com>
2018-04-08 12:14:29 -0600

    add in footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    Made by <a href="https://twitter.com/sarah_edo" target="_blank">sarah_edo</a>. This project is open source, visit <a href="https://github.com/sdras/page-transitions-travelapp" target="_blank">the repo.</a>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  background: black;
+  color: white;
+  text-align: center;
+  letter-spacing: 0.03em;
+  margin-top: 30px;
+  width: 100%;
+  height: 50px;
+}
+
+a,
+a:visited,
+a:active {
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+  padding-left: 6px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div>
     <app-navigation />
     <nuxt/>
+    <app-footer />
   </div>
 </template>
 
 <script>
 import AppNavigation from '~/components/AppNavigation.vue'
+import AppFooter from '~/components/AppFooter.vue'
 
 export default {
   components: {
-    AppNavigation
+    AppNavigation,
+    AppFooter
   }
 }
 </script>
